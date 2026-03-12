4df23ec55b6e0893e5c1ddb55e90d532c1366ffc
udupa <uduparohan@gmail.com>
2026-03-12 13:01:05 -0700

    Adding fhir and backup lambda

--- lambda/public-api/public-api-lambdas.ts
+++ lambda/public-api/public-api-lambdas.ts
@@ -5,6 +5,8 @@ import { Construct } from "constructs";
 import { Runtime, Code } from "aws-cdk-lib/aws-lambda";
 import { Topic } from "aws-cdk-lib/aws-sns";
 import { Bucket } from "aws-cdk-lib/aws-s3";
+import { S3EventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { EventType } from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 interface FibulaLambdasProps {
@@ -13,6 +15,11 @@ interface FibulaLambdasProps {
   installerBucket: Bucket;
   domainName: string;
   dbDumpBucket: Bucket;
+  rdsHost?: string;
+  rdsPort?: string;
+  rdsDbName?: string;
+  rdsUsername?: string;
+  rdsPassword?: string;
 }
 
 export class FibulaLambdas extends Construct {
@@ -23,6 +30,7 @@ export class FibulaLambdas extends Construct {
   readonly getEnrollmentRequestsLambdas: Function;
   readonly getInstallerLambda: Function;
   readonly uploadDump: Function;
+  readonly fhirProcessor: Function;
 
   constructor(scope: Construct, id: string, props: FibulaLambdasProps) {
     super(scope, id);
@@ -135,9 +143,52 @@ export class FibulaLambdas extends Construct {
       handler: "upload_dump.lambda_handler",
       environment: {
         BUCKET_NAME: props.dbDumpBucket.bucketName,
+        API_KEY: "femr-local-dev-api-key-12345-change-in-production",
       },
     });
 
     props.dbDumpBucket.grantPut(this.uploadDump);
+
+    // FHIR Processor Lambda - triggered by S3 uploads
+    this.fhirProcessor = new Function(scope, "FHIRProcessorLambda", {
+      runtime: Runtime.PYTHON_3_13,
+      code: Code.fromAsset("lambda/fhir-processor", {
+        bundling: {
+          image: Runtime.PYTHON_3_13.bundlingImage,
+          command: [
+            "bash",
+            "-c",
+            "pip install -r requirements.txt -t /asset-output && cp -au . /asset-output",
+          ],
+        },
+      }),
+      handler: "process_sql_to_fhir.lambda_handler",
+      timeout: cdk.Duration.minutes(15), // SQL processing can take time
+      memorySize: 1024, // Increased memory for FHIR processing
+      environment: {
+        RDS_HOST: props.rdsHost || "",
+        RDS_PORT: props.rdsPort || "5432",
+        RDS_DB_NAME: props.rdsDbName || "",
+        RDS_USERNAME: props.rdsUsername || "",
+        RDS_PASSWORD: props.rdsPassword || "",
+      },
+    });
+
+    // Grant S3 read permissions to FHIR processor
+    props.dbDumpBucket.grantRead(this.fhirProcessor);
+
+    // Add S3 event trigger for FHIR processor
+    const s3EventSource = new S3EventSource(
+      props.dbDumpBucket,
+      {
+        events: [EventType.OBJECT_CREATED],
+        filters: [
+          { suffix: ".sql" },
+          { suffix: ".sql.gz" },
+        ],
+      }
+    );
+
+    this.fhirProcessor.addEventSource(s3EventSource);
   }
 }
